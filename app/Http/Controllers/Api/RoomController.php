56ba3fd331ebbbf9848a60d3c20d7775975b2a3b
junzmint <junzmint07112002@gmail.com>
2023-12-11 22:55:06 +0700

    fea: update room api

--- app/Http/Controllers/Api/RoomController.php
+++ app/Http/Controllers/Api/RoomController.php
@@ -52,23 +52,37 @@ class RoomController extends Controller
 
     public function addTentantToRoom(Request $request, string $id)
     {
-        $tenant = Tenant::create([
-            'name' => $request->input('name'),
-            'phone_number' => $request->input('phone_number'),
-            'citizen_number' => $request->input('citizen_number'),
-            'gender' => $request->input('gender'),
-            'email' => $request->input('email'),
-        ]);
-        $room = Room::where('id', $id)->first();
+        if (Tenant::where('citizen_number', $request->input('citizen_number'))->first() != null) {
+            $tenant = Tenant::where('citizen_number', $request->input('citizen_number'));
 
-        $room->tenants()->attach($tenant, [
-            'room_host' => $request->input('room_host'),
-            'rent_type' => $request->input('rent_type'),
-            'living_status' => $request->input('living_status'),
-            'created_at' => now(),
-        ]);
+            $room = Room::where('id', $id)->first();
+            $room->tenants()->attach($tenant, [
+                'room_host' => $request->input('room_host'),
+                'rent_type' => $request->input('rent_type'),
+                'living_status' => $request->input('living_status'),
+                'created_at' => now(),
+            ]);
 
-        return $tenant;
+            return $tenant;
+        } else {
+            $tenant = Tenant::create([
+                'name' => $request->input('name'),
+                'phone_number' => $request->input('phone_number'),
+                'citizen_number' => $request->input('citizen_number'),
+                'gender' => $request->input('gender'),
+                'email' => $request->input('email'),
+            ]);
+            $room = Room::where('id', $id)->first();
+
+            $room->tenants()->attach($tenant, [
+                'room_host' => $request->input('room_host'),
+                'rent_type' => $request->input('rent_type'),
+                'living_status' => $request->input('living_status'),
+                'created_at' => now(),
+            ]);
+
+            return $tenant;
+        }
     }
 
     public function DeleteTentantInRoom(string $id, string $tenant_id)
